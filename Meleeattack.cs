20e9862b866b2f95989a900626b6fcc663d7ee57
imsenthur <imsenthur@gmail.com>
2017-10-25 16:10:07 +0530

    Add files via upload

---
+++ Meleeattack.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meleeattack : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<DamageHandler>().health--;
+    }
+}
